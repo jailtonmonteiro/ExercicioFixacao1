28d9012722cce4d39f22c6476c8a9c88997b9690
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-23 13:55:33 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioFixacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Digite largura e altura: ");
+            rectangle.Width = double.Parse(Console.ReadLine());
+            rectangle.Height = double.Parse(Console.ReadLine());
+
+            rectangle.calculateArea();
+            rectangle.calculatePerimeter();
+            rectangle.calculateDiagonal();
+        }
+    }
+}
---
+++ ExercicioFixacao1/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao1
+{
+    internal class Rectangle
+    {
+        double width, height, area, perimeter, diagonal;
+
+        public void calculateArea()
+        {
+            area = width * height;
+            Console.WriteLine($"AREA: {area.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        public void calculatePerimeter()
+        {
+            perimeter = (width * 2) + (height * 2);
+            Console.WriteLine($"PERIMETRO: {perimeter.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        public void calculateDiagonal()
+        {
+            diagonal = Math.Sqrt((width * width) + (height * height));
+            Console.WriteLine($"DIAGONAL: {diagonal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+        public double Perimeter
+        {
+            get { return perimeter; }
+            set { perimeter = value; }
+        }
+        public double Diagonal
+        {
+            get { return diagonal; }
+            set { diagonal = value; }
+        }
+    }
+}
